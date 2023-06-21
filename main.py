0b0002efd28eec1759b443466605bc8cf6df9b55
Joshua <joshua.beck.0908@gmail.com>
2023-06-21 19:45:34 +0930

    Updated interface and collision optimizations.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import random
-from re import L
-
 import vectorio
 import terminalio
 import board
@@ -19,6 +17,7 @@ DISPLAY_HEIGHT = 240
 FPS = 30
 
 gameOver = False
+victory = False
 allObjects = displayio.Group()
 allSprites = []
 
@@ -100,6 +99,8 @@ class Object:
     SOT_PART = 4
     CHIP_PART = 5
     PLAYER_PROJECTILE = 6
+    ENEMY_2 = 7
+    ENEMY_3 = 8
     
 class UIDisplay:
     score = 0
@@ -113,6 +114,44 @@ class UIDisplay:
     
     def __init__(self):
         self.reset()
+        
+    def pauseWithText(self, text):
+        gameMusic.pause()
+        pauseText = self.addCentreText(text)
+        board.DISPLAY.refresh()
+        while buttonPad.anyKeyPressed():
+            time.sleep(0.1)
+        while True:
+            if buttonPad.anyKeyPressed():
+                break
+            time.sleep(0.1)
+        gameMusic.resume()
+        allObjects.remove(pauseText)
+        board.DISPLAY.refresh()
+
+    def addCentreText(self, text):
+        global allObjects
+        text = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
+        text.x = DISPLAY_WIDTH // 2 - text.width // 2
+        text.y = DISPLAY_HEIGHT // 2 - text.height // 2
+        allObjects.append(text)
+        return text
+
+    def spawnPart(self, sprite, bonus):
+        if sprite.type == Object.ENEMY:
+            partType = Object.PASSIVE_PART
+        elif sprite.type == Object.ENEMY_2:
+            partType = Object.SOT_PART
+        elif sprite.type == Object.ENEMY_3:
+            partType = Object.CHIP_PART
+        else:
+            return
+        
+        createProjectile(sprite.pos, partType, [0, 2])
+        if bonus:
+            createProjectile([sprite.pos[0], sprite.pos[1] + 20], partType, [0, 2])
+        
+        
         
     def reset(self):
         global allObjects, spriteSheet, spritePalette
@@ -135,6 +174,9 @@ class UIDisplay:
             self.quoteSprite.append(Sprite(spriteSheet, spritePalette, Object.PASSIVE_PART + i, [GAME_EDGE + 5, 50 + i * 30], uiPart=True))
             self.quoteLabel.append(label.Label(terminalio.FONT, text=f'x{self.quota[i]:03}', color=0xFF0000, x=GAME_EDGE + 5, y=75 + i * 30))
             self.objects.append(self.quoteLabel[i])
+        self.scoreUpdate = False
+        self.livesUpdate = False
+        self.quotaUpdate = False
             
         
     def loseLife(self):
@@ -142,24 +184,95 @@ class UIDisplay:
         self.lives -= 1
         if self.lives <= 0:
             gameOver = True
+        self.livesUpdate = True
+                
+    def setupQuota(self):
+        self.quota = gameLevel.quota
+        self.quotaUpdate = True
+    
+    def collectPart(self, part):
+        global victory
+        if self.quota[part] > 0:
+            self.quota[part] -= 1
+        self.quotaUpdate = True
             
-    def setQuota(self, level):
-        pass
+        victory = True
+        for i in range(3):
+            if self.quota[i] > 0:
+                victory = False
+
     def startLevel(self):
-        self.setQuota(self.level)        
+        self.setupQuota()
+        self.pauseWithText(f'Level {self.level}: {gameLevel.name}')
+        
+    def addPoints(self, number):
+        self.score += number
+        self.scoreUpdate = True
 
     def update(self):
-        self.scoreLabel.text = "Score: " + str(self.score)
-        self.livesLabel.text = f'x {self.lives}'
+        if self.scoreUpdate:
+            self.scoreLabel.text = "Score: " + str(self.score)
+            self.scoreUpdate = False
+        if self.livesUpdate:
+            self.livesLabel.text = f'x {self.lives}'
+            self.livesUpdate = False
+        if self.quotaUpdate:
+            for part in range(3):
+                self.quoteLabel[part].text = f'x{self.quota[part]:03}'
+            self.quotaUpdate = False
 
 class Level:
     data = {
-        1: {'name': 'Basics',           'quota': [10, 0, 0],    'enemyMax': 3},
-        2: {'name': 'Balanced Boards',  'quota': [10, 5, 2],    'enemyMax': 4},
-        3: {'name': 'Chip Shortage',    'quota': [0, 0, 5],     'enemyMax': 5}
+        1: {
+            'name': 'Basics',
+            'quota': [5, 0, 0],
+            'enemyMax': [2, 0, 0],
+            'enemyFrequency': [0.5, 0.0, 0.0]
+        }, 
+
+        2: {
+            'name': 'Balanced Boards',
+            'quota': [5, 5, 0],
+            'enemyMax': [2, 2, 0],
+            'enemyFrequency': [0.5, 0.5, 0.0]
+        },
+
+        3: {
+            'name': 'Chip Shortage',
+            'quota': [0, 0, 5],
+            'enemyMax': [0, 2, 2],
+            'enemyFrequency': [0.0, 0.5, 0.5]
+        },
+        4: {
+            'name': 'The Full Stack',
+            'quota': [3, 3, 3],
+            'enemyMax': [2, 2, 2],
+            'enemyFrequency': [0.5, 0.5, 0.5]
+        },
+        5: {
+            'name': 'I only need one',
+            'quota': [1, 0, 0],
+            'enemyMax': [1, 2, 2],
+            'enemyFrequency': [0.05, 0.5, 0.5]
+        },
+        6: {
+            'name': 'Gotta Lotta SOT',
+            'quota': [0, 25, 0],
+            'enemyMax': [0, 2, 2],
+            'enemyFrequency': [0.4, 0.7, 0.5]
+        },
+        7: {
+            'name': 'Maximum Capacity',
+            'quota': [30, 0, 0],
+            'enemyMax': [2, 3, 3],
+            'enemyFrequency': [0.5, 0.5, 0.5]
+        }
     }
     current = 0
 
+
+    def reset(self):
+        self.current = 0
 
     def set(self, level):
         self.current = level
@@ -167,6 +280,10 @@ class Level:
         self.name = levelData['name']
         self.quota = levelData['quota']
         self.enemyMax = levelData['enemyMax']
+        
+    def next(self):
+        self.current += 1
+        self.set(self.current)
         
         
 class Input:
@@ -202,13 +319,27 @@ class Sprite:
         self.sprite[0] = spriteIndex
         self.sprite.x = position[0]
         self.sprite.y = position[1]
+        self.delta = [0, 0]
+        self.oldpos = position
         self.pos = position
         self.velocity = [0, 0]
         self.type = spriteIndex
         self.uiPart = uiPart
+        self.moveRoutine = None
+        self.collisionRoutine = None
         allObjects.insert(0, self.sprite)
         allSprites.append(self)
         
+    def collide(self, other):
+        if self.uiPart or other.uiPart:
+            return False
+        if self.collisionRoutine:
+            return self.collisionRoutine(self, other)
+    
+    def movement(self):
+        if self.moveRoutine:
+            self.moveRoutine(self)
+    
     def move(self, xmove, ymove):
         if self.uiPart:
             return
@@ -230,10 +361,13 @@ class Sprite:
         self.isDead = True
         
     def update(self):
+        self.oldpos[0] = self.pos[0]
+        self.oldpos[1] = self.pos[1]
         self.frame += 1
         self.move(*self.velocity)
-        if self.moveRoutine:
-            self.moveRoutine(self)
+        self.movement()
+        self.delta[0] = self.pos[0] - self.oldpos[0]
+        self.delta[1] = self.pos[1] - self.oldpos[1]
             
 def checkOverlap(sprite1, sprite2):
     if sprite1.pos[0] > sprite2.pos[0] + sprite2.size[0] or sprite1.pos[0] + sprite1.size[0] < sprite2.pos[0]:
@@ -245,19 +379,21 @@ def checkOverlap(sprite1, sprite2):
     
 def checkCollisions():
     for sprite in allSprites:
-        if sprite.uiPart:
+        if sprite.uiPart or sprite.isDead or sprite.delta == [0, 0]:
             continue
-        for collisionSprite in allSprites:
-            if sprite != collisionSprite and checkOverlap(sprite, collisionSprite):
-                if sprite.collisionRoutine:
-                    sprite.collisionRoutine(sprite, collisionSprite)
+        for otherSprite in allSprites:
+            if sprite == otherSprite or otherSprite.isDead or otherSprite.collisionRoutine == None:
+                continue
+            if checkOverlap(sprite, otherSprite):
+                sprite.collide(otherSprite)
+                otherSprite.collide(sprite)
                     
 def updateSprites():
-    checkCollisions()
     for sprite in allSprites:
         sprite.update()
     for sprite in allSprites:
         removeIfDead(sprite)
+    checkCollisions()
         
 def removeIfDead(sprite):
     if sprite.isDead:
@@ -281,16 +417,14 @@ def enemyCollisionRoutine(sprite, collisionSprite):
     if collisionSprite.type == Object.PLAYER_PROJECTILE:
         sprite.kill()
         collisionSprite.kill()
-        projRoll = random.randint(0, 100)
-        if projRoll < 60:
-            pass
-        elif projRoll < 80:
-            createProjectile([sprite.sprite.x, sprite.sprite.y + 20], Object.PASSIVE_PART, [0, 2])
-        elif projRoll < 90:
-            createProjectile([sprite.sprite.x, sprite.sprite.y + 20], Object.SOT_PART, [0, 2])
-        elif projRoll < 95:
-            createProjectile([sprite.sprite.x, sprite.sprite.y + 20], Object.CHIP_PART, [0, 2])
-        return True
+        rand = random.random()
+            
+        if rand > 0.5:
+            gameUi.spawnPart(sprite, bonus=False)
+        elif rand > 0.9:
+            gameUi.spawnPart(sprite, bonus=True)
+        
+            
     return False
 
 def playerCollisionRoutine(sprite, collisionSprite):
@@ -304,25 +438,25 @@ def playerCollisionRoutine(sprite, collisionSprite):
         return True
     elif collisionSprite.type == Object.PASSIVE_PART:
         collisionSprite.kill()
-        gameUi.score += 5
-        gameUi.quota[0] -= 1
+        gameUi.collectPart(0)
+        gameUi.addPoints(5)
         return True
     elif collisionSprite.type == Object.SOT_PART:
         collisionSprite.kill()
-        gameUi.score += 10
-        gameUi.quota[1] -= 1
+        gameUi.collectPart(1)
+        gameUi.addPoints(10)
         return True
     elif collisionSprite.type == Object.CHIP_PART:
         collisionSprite.kill()
-        gameUi.score += 25
-        gameUi.quota[2] -= 1
+        gameUi.collectPart(2)
+        gameUi.addPoints(25)
         return True
     else:
         return False
 
 def enemyMoveRoutine(sprite):
     if random.randint(0, 100) == 0:
-       createProjectile([sprite.sprite.x, sprite.sprite.y + 20], Object.ENEMY_PROJECTILE, [0, 2])
+        createProjectile([sprite.sprite.x, sprite.sprite.y + 20], Object.ENEMY_PROJECTILE, [0, 2])
     if sprite.frame % 4 < 2:
         return
     if sprite.velocity[0] == 0:
@@ -350,34 +484,15 @@ gameMusic = MusicPlayer(backgroundMusic, board.SPEAKER, loop=True, volume=4)
 gameUi = UIDisplay()
 gameLevel = Level()
 
-def addCentreText(text):
-    global allObjects
-    text = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
-    text.x = DISPLAY_WIDTH // 2 - text.width // 2
-    text.y = DISPLAY_HEIGHT // 2 - text.height // 2
-    allObjects.append(text)
-    return text
-
-def pauseGame():
-    gameMusic.pause()
-    pauseText = addCentreText("PAUSED")
-    while buttonPad.anyKeyPressed():
-        time.sleep(0.1)
-    while True:
-        if buttonPad.anyKeyPressed():
-            break
-        time.sleep(0.1)
-    gameMusic.resume()
-    allObjects.remove(pauseText)
-
 def main():
-    global spriteSheet, spritePalette, gameOver
+    global spriteSheet, spritePalette, gameOver, victory
     board.DISPLAY.root_group = allObjects
+    board.DISPLAY.auto_refresh = False
     lastFrame = time.monotonic()
     while True:
         player = Sprite(spriteSheet, spritePalette, 0, [GAME_EDGE // 2, 200])
         player.assignCollisionRoutine(playerCollisionRoutine)
-        gameLevel.set(1)
+        gameLevel.next()
         enemies = []
         enemyMax = 3
         firing = False
@@ -390,7 +505,7 @@ def main():
             if buttonPad.isPressed(Input.LEFT):
                 if buttonPad.isPressed(Input.RIGHT):
                     player.velocity[0] = 0
-                    pauseGame()
+                    gameUi.pauseWithText("PAUSED")
                 player.velocity[0] = -2
             elif buttonPad.isPressed(Input.RIGHT):
                 player.velocity[0] = 2
@@ -410,29 +525,36 @@ def main():
             updateSprites()
             gameMusic.update()
             gameUi.update()
+            board.DISPLAY.refresh()
             frameDelay = 1 / FPS - (time.monotonic() - lastFrame)
             if frameDelay > 0:
                 time.sleep(frameDelay)
+                
+            if victory:
+                gameUi.addCentreText("VICTORY")
+                deleteAll()
+                gameLevel.next()
+                victory = False
+                gameUi.reset()
+                gameMusic.play()
         gameOverScreen()
         
 
 def pressToStart():
-    gameMusic.pause()
-    startText = addCentreText("PRESS ANY BUTTON TO START")
-    while not buttonPad.anyKeyPressed():
-        time.sleep(0.01)
-    allObjects.remove(startText)
-    gameMusic.resume()
+    gameUi.pauseWithText("PRESS ANY BUTTON TO START")
     
 def gameOverScreen():
     global gameOver
+    gameUi.pauseWithText("GAME OVER")
     gameMusic.stop()
-    addCentreText("GAME OVER")
+    board.DISPLAY.refresh()
     while not buttonPad.anyKeyPressed():
         time.sleep(0.01)
+    board.DISPLAY.refresh()
     gameOver = False
     deleteAll()
     gameUi.reset()
+    gameLevel.reset()
 
 if __name__ == "__main__":
     main()
